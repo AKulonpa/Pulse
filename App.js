920a83e458d1c5bdc946fdc6185cca4fabb480b3
Alisa <112699771+AKulonpa@users.noreply.github.com>
2023-09-12 15:25:24 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [num1, setNum1] = useState ('')
+  const result1 = (220 -num1)* 0.65
+  const result2 = (220 -num1)* 0.85 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Heart rate limits</Text>
+      <Text style={styles.text}>Enter age</Text>
+      <TextInput
+        keyboardType='numeric'
+        value={num1}
+        onChangeText={text => setNum1(text)}
+        style={styles.textInput}
+      />
+      <Text style={styles.text}>Hr limits</Text>
+      {num1 ? <Text>{result1.toFixed(0)}</Text> : null}
+      {num1 ? <Text>{result2.toFixed(0)}</Text> : null}
     </View>
   );
 }
@@ -16,5 +30,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  header: {
+    marginBottom: 30,
+    fontSize: 24,
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 8, 
+    width: 50,
+    height: 30,
+  },
+  text:{
+    fontSize: 20,
+  }
 });
